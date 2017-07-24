64f7397be09d75ee68f9943760fa2717ca3881f7
Leonardo G. Trombetta <lgtrombetta@gmx.com>
2017-07-24 10:42:27 -0300

    Update timelapse.py

--- timelapse.py
+++ timelapse.py
@@ -24,9 +24,11 @@ u.close()
 now = datetime.datetime.now()
 
 basedir = '/home/pi/Python/Timelapse-catedral/'
-dir = basedir+'images/'+str(now.year)+'/'+str2(now.month)+'/'+str2(now.day)
+dir = basedir+str(now.year)+'/'+str2(now.month)+'/'+str2(now.day)+'/images'
 call(['mkdir', '-p', dir])
 
 f = open(dir+'/'+str(now.year)+'-'+str2(now.month)+'-'+str2(now.day)+'_'+str2(now.hour)+'-'+str2(now.minute)+'-'+str2(now.second)+'.jpg','wb')
 f.write(raw_data)
 f.close()
+
+"""test"""
