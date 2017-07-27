c1a962372be5318b0c1e26b44ab8b758d859ff20
Marco Trombetta <marcoltrombetta@gmail.com>
2017-07-27 11:47:48 -0300

    list dir

--- viewer/app.py
+++ viewer/app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, url_for
 from subprocess import call
 from shutil import move
 from time import sleep
-
-
+import os, sys
+import json
 app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
@@ -12,12 +12,12 @@ def index():
 
 @app.route('/capture/')
 def capture():
-    return render_template('capture.html')
-    call(["gphoto2","--capture-image-and-download"])
-    sleep(2)
-    move("capt0000.jpg", "static/img/capt0000.jpg")
-    sleep(1)
-    return render_template('capture.html')
+    mypath='/home/marco/Desktop'
+    onlyfiles = [f for f in os.listdir(mypath) if os.path.isfile(os.path.join(mypath, f))]
+
+    data=json.dumps(onlyfiles,ensure_ascii=False)
+
+    return (data)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
