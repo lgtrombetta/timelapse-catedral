a91a39f3cbf8fbba1d63ca1bc778678c8d7760ff
Leonardo G. Trombetta <lgtrombetta@gmx.com>
2017-07-27 20:36:39 -0300

    update

--- viewer.py
+++ viewer.py
@@ -6,39 +6,51 @@ Web viewer
 """
 
 import os
+import datetime
 import json
 from flask import Flask, render_template, url_for
 
 
-app = Flask(__name__, static_url_path='/static')
+def str2(num):
+    """Completes one-digit numbers with a preceding 0"""
+    if num < 10:
+        return str(0)+str(num)
+    return str(num)
 
 
 home_dir = os.path.expanduser("~")
 base_dir = open(home_dir+'/.timelapse-catedral/basedir.txt', 'r').read()
 base_dir = base_dir.strip()
 
-if base_dir[-1] != '/':
-    base_dir = base_dir+'/'
+
+viewer = Flask(__name__, static_url_path='/static')
 
 
-@app.route('/')
+@viewer.route('/')
 def index():
     """Main page"""
     return render_template('index.html')
 
 
-@app.route('/capture/')
-def capture():
-    """Capture"""
-    """mypath = '/home/marco/Desktop'
+@viewer.route('/today/')
+def today():
+    """Show today's video and pictures"""
+    now = datetime.datetime.now()
 
-    onlyfiles = [f for f in os.listdir(mypath) if
-                 os.path.isfile(os.path.join(mypath, f))]
+    today_rp = os.path.join('files', str(now.year), str2(now.month),
+                            str2(now.day))
+    today_rp_img = os.path.join(today_rp, 'images')
 
-    data = json.dumps(onlyfiles, ensure_ascii=False)"""
+    today_ap_img = os.path.join(base_dir, 'static', today_rp_img)
 
-    ###return data
-    return render_template('capture.html')
+    images = [url_for('static', filename=os.path.join(today_rp_img, f))
+              for f in os.listdir(today_ap_img) if
+              os.path.isfile(os.path.join(today_ap_img, f))]
+
+    data = json.dumps(images, ensure_ascii=False)
+
+    return render_template('show.html', data=data)
+
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    viewer.run(debug=True, host='0.0.0.0')
