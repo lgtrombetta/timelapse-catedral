dcefd3382af6f58c7900258eebbcfbf733a508ef
Leonardo G. Trombetta <lgtrombetta@gmx.com>
2017-07-27 23:22:00 -0300

    update

--- viewer.py
+++ viewer.py
@@ -37,32 +37,34 @@ def today():
     """Show today's video and pictures"""
     now = datetime.datetime.now()
 
-    today_rp = os.path.join('files', str(now.year), str2(now.month),
-                            str2(now.day))
-    today_ap = os.path.join(base_dir, 'static', today_rp)
-
-    today_rp_img = os.path.join(today_rp, 'images')
-    today_ap_img = os.path.join(today_ap, 'images')
-
-    images = [url_for('static', filename=os.path.join(today_rp_img, f))
-              for f in os.listdir(today_ap_img) if
-              os.path.isfile(os.path.join(today_ap_img, f))]
-
-    video = [url_for('static', filename=os.path.join(today_rp, f))
-             for f in os.listdir(today_ap) if
-             os.path.isfile(os.path.join(today_ap, f))]
-
-    data = json.dumps({'images': images, 'video': video[0]},
-                      ensure_ascii=False)
-
-    return render_template('show.html', data=data)
+    return show(str(now.year), str2(now.month), str2(now.day))
+    # redirect(os.path.join(str(now.year), str2(now.month), str2(now.day)),
+    # code=302)
 
 
 @viewer.route('/<year>/<month>/<day>/')
 def show(year, month, day):
-    print(os.path.join(year, month, day))
+    """Show pictures and video of a given day"""
+    rel_path = os.path.join('files', year, month, day)
+    abs_path = os.path.join(base_dir, 'static', rel_path)
 
-    return render_template('show.html', data=os.path.join(year, month, day))
+    rp_img = os.path.join(rel_path, 'images')
+    ap_img = os.path.join(abs_path, 'images')
+
+    images = [url_for('static', filename=os.path.join(rp_img, f))
+              for f in os.listdir(ap_img) if
+              os.path.isfile(os.path.join(ap_img, f))]
+
+    video = [url_for('static', filename=os.path.join(rel_path, f))
+             for f in os.listdir(abs_path) if
+             os.path.isfile(os.path.join(abs_path, f))]
+
+    data = json.dumps({'date': os.path.join(year, month, day),
+                       'images': images, 'video': video[0]},
+                      ensure_ascii=False)
+
+    return render_template('show.html', data=data)
+
 
 if __name__ == '__main__':
     viewer.run(debug=True, host='0.0.0.0')
